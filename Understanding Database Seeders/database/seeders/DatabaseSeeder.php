8abe83828b4a155e84c4c479fb802d643a918b9d
Mostafa <mostafamahmoud805554@std.mans.edu.eg>
2024-10-07 14:51:35 +0300

    Understanding Database Seeders

--- Understanding Database Seeders/database/seeders/DatabaseSeeder.php
+++ Understanding Database Seeders/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'Mustafa',
+            'last_name' => 'Mahmoud',
+            'email' => 'disney@example.com',
         ]);
+
+        $this->call(JobSeeder::class);
     }
 }
